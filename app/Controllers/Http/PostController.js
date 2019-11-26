df29c56e4ef8d55130c322f9d91fee5baf4685ab
Labib <labibizzr@gmail.com>
2019-11-26 08:57:04 +0700

    video 3

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,18 +1,23 @@
 'use strict'
 
+const Post = use('App/Models/Post')
+
 class PostController {
   async index({ view }){
 
-    const posts =[
-      { title : 'Post One', body : 'This is post one' },
-      { title : 'Post Two', body : 'This is post two' },
-      { title : 'Post Three', body : 'This is post three' }
-    ]
+    // const posts =[
+    //   { title : 'Post One', body : 'This is post one' },
+    //   { title : 'Post Two', body : 'This is post two' },
+    //   { title : 'Post Three', body : 'This is post three' }
+    // ]
 
+   const posts = await Post.all()
+
+    console.dir(posts)
     return view.render('posts.index', {
       title : 'Latest Posts',
       ayaya : 'ayaya',
-      posts : posts
+      posts : posts.toJSON()
     })
   }
 
